11ebef802856247dfd4cf40b4a4cadc2f24261da
Kevin <kevinkhew@convep.com>
2015-03-13 22:13:38 +0800

    change the way of logs file being saved

--- app/start/global.php
+++ app/start/global.php
@@ -48,15 +48,24 @@ Log::useFiles(storage_path().'/logs/laravel.log');
 
 App::error(function(Exception $exception, $code)
 {
-	// for local debug use
-	// Log::error($exception);
 
 	switch($code)
 	{
-		case 403: return Response::view('errors.403', array(), 403);
-		case 404: return Response::view('errors.404', array(), 404);
-		case 500: return Response::view('errors.500', array(), 500);
-		default: return Response::view('errors.default', array(), $code);
+		case 403: {
+			Log::error($exception);
+			return Response::view('errors.403', array(), 403);
+		}
+		case 404: {
+			return Response::view('errors.404', array(), 404);
+		}
+		case 500: {
+			Log::error($exception);
+			return Response::view('errors.500', array(), 500);
+		}
+		default: {
+			Log::error($exception);
+			return Response::view('errors.default', array(), $code);
+		}
 	}
 	
 });
